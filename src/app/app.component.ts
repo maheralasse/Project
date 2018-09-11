7b23c75f28ab1aeb9208e0d9093bf5bf22087d74
maheralasse <40716673+maheralasse@users.noreply.github.com>
2018-09-11 17:53:06 +0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import{HomeComponent} from './home/home.component' 
+import{ThreeComponent} from './three/three.component' 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  
+})
+export class AppComponent {
+  title = 'Albaath University';
+}
